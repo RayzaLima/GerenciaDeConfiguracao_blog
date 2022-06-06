4f2b6f8fb72ed68cf1827a08007ff1376a78328b
RayzaLima <rayzalima2@gmail.com>
2022-06-05 22:29:46 -0300

    First Commit - Gerencia_Blog

---
+++ src/main/java/com/gerenciadeconfiguracao_blog/GerenciaDeConfiguracaoBlogApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciadeconfiguracao_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciaDeConfiguracaoBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciaDeConfiguracaoBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GerenciaDeConfiguracao_blog/GerenciaDeConfiguracaoBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GerenciaDeConfiguracao_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciaDeConfiguracaoBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
